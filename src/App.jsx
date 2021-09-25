2755aee7c66f3fc5f891e510f2489d5d1fd52ecc
Sumon Chhetry <sumonchhetry1@gmail.com>
2021-09-25 23:05:00 +0530

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,39 @@ import React from "react";
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
-const App = (props) => {
+const App = () => {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
+  const addExpenseHandler = (expense) => {
+    console.log("in App.jsx");
+    console.log(expense);
+  };
+
   // An alternate approach
   // return React.createElement(
   //   "div",
@@ -14,8 +46,8 @@ const App = (props) => {
 
   return (
     <div>
-      <NewExpense />
-      <Expenses items={props.Expenses} />
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
 };
