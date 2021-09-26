aaf5e23a626172605c2a3c2605f84b3d562ae002
Sumon Chhetry <sumonchhetry1@gmail.com>
2021-09-26 22:19:54 +0530

    fourth commit

--- src/components/Expenses/ExpenseDate.jsx
+++ src/components/Expenses/ExpenseDate.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./expensedate.css";
 
 const ExpenseDate = (props) => {
-  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const month = props.date.toLocaleString("en-US", { month: "short" });
   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
   const year = props.date.getFullYear();
   return (
